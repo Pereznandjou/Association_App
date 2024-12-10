7255488c2512325eaf92a98b01b3ef89bdff0d88
Wendy <wendy.alphanor@outlook.fr>
2024-12-10 10:20:53 +0100

    delt

---
+++ Association_Project/Association_Project/Association_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Association_ProjectApp.swift
+//  Association_Project
+//
+//  Created by Wendy ALPHANOR on 10/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Association_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Association_Project/Association_ProjectTests/Association_ProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  Association_ProjectTests.swift
+//  Association_ProjectTests
+//
+//  Created by Wendy ALPHANOR on 10/12/2024.
+//
+
+import Testing
+@testable import Association_Project
+
+struct Association_ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
